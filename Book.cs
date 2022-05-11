83559797fc5875e862dc921b6d988aaa01cad24a
Isyaka Ibrahim Maikudi <63717663+Techebraheem@users.noreply.github.com>
2022-05-11 12:09:16 +0100

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Book
+    {
+        //attributes of a class Book
+
+        public string title;
+        public string author;
+        public int pages;
+
+        //using constructors to illustrate
+        public Book(string aTitle, string aAuther, int aPages)
+        {
+            title = aTitle;
+            author = aAuther;
+            pages = aPages;
+        }
+    }
+    class Programs
+    {
+        static void Main (string[] args)
+        {
+
+            // instatiating an object
+
+            Book book1 = new Book("Harry Potter", "JK Rowlling", 400);
+
+            Book book2 = new Book("Lord of the rings", "Tolkien", 700);
+
+            Console.WriteLine(book1.title);
+        }    
+    }
+}
