b7634af16efd10cf3247819e8f90a2b55cebb859
Isyaka Ibrahim Maikudi <63717663+Techebraheem@users.noreply.github.com>
2022-05-12 07:40:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public enum ProductCodes
+        {
+            //enum illustration
+
+            Milk = 0,
+            Juice = 1, 
+            Tea = 2
+        }
+        static void Main (String[] args)
+        {
+            ProductCodes test = ProductCodes.Milk;
+
+           Console.WriteLine ((int)test);
+           
+        }
+    }
+}
+   
+
+    
+ 
